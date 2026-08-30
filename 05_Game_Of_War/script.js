42548812c600f0cb38345f149e70ca4b6ce31b48
thidassandaruwan <170103578+thidassandaruwan@users.noreply.github.com>
2026-08-30 21:59:19 +0530

    GAME OF WAR : Add the files

---
+++ 05_Game_Of_War/script.js
@@ -0,0 +1,4 @@
+// "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
+// `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
+
+const DECK_OF_CARDS_API = "https://apis.scrimba.com/deckofcards/api/deck/";
